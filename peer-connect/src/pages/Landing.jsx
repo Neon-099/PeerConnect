48fc86c34a2066b37ad36f77a27448c904d6079b
Neon-099 <Emmannatividad099@gmail.com>
2025-09-10 20:31:56 +0800

    empty files

---
+++ peer-connect/src/pages/Landing.jsx
@@ -0,0 +1,21 @@
+import {GraduationCap} from 'lucide-react';
+
+
+const Landing = () => {
+    
+    
+    
+    
+    
+    
+    
+    return (
+        <div>
+            <header>
+                
+            </header>
+        </div>
+    )
+}
+
+export default Landing;
