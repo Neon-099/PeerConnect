87ea29522483f1ab94e5bbde8fc52d8ec2ec8c2e
Neon-099 <Emmannatividad099@gmail.com>
2025-09-17 23:23:15 +0800

    change file path location

--- peer-connect/src/App.jsx
+++ peer-connect/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Landing from './pages/Landing.jsx';
+import Landing from './pages/Student/Landing.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
