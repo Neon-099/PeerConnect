fd630028cdc4b06f1d59127287eeb24dc9aded99
Neon-099 <Emmannatividad099@gmail.com>
2025-09-21 23:59:43 +0800

    auth form sign up

--- peer-connect/src/App.jsx
+++ peer-connect/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Landing from './pages/Student/Landing.jsx';
 import StudentAuthForm from './pages/auth/StudentAuthForm.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const googleClientId = import.meta.env.GOOGLE_CLIENT_ID;
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />}/>
-        <Route path="/signup" element={<StudentAuthForm />}/>
-      </Routes>
-    </Router>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />}/>
+          <Route path="/signup" element={<StudentAuthForm />}/>
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   )
 }
 
